dc3ea2e81b04a7c1855e3dbbc3ab9fb15190e5cc
RKovl <ryan.kovlaske@gmail.com>
2023-04-27 14:44:43 -0700

    scraped wiki data

--- src/pages/temp/currentEvents.js
+++ src/pages/temp/currentEvents.js
@@ -1,12 +1,31 @@
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
+
 
 export default function currentEvents() {
-    return ( 
-    <>
-        <h1>First Post</h1>
-        <h2>
-            <Link href="/">Back</Link>
-        </h2>
-    </>
+    const [content, setContent] = useState('');
+
+    useEffect(() => {
+      // Define handleResponse function inside component
+      window.handleResponse = (response) => {
+        let indexOfContent = response.parse.text['*'].indexOf('<div class="current-events-content description">')
+        console.log(indexOfContent)
+        setContent(response.parse.text['*'].slice(indexOfContent));
+      };
+  
+      const url = "https://en.wikipedia.org/w/api.php?action=parse&page=Portal:Current_events&format=json&callback=handleResponse";
+      const script = document.createElement("script");
+      script.src = url;
+      document.body.appendChild(script);
+  
+      return () => {
+        document.body.removeChild(script);
+        // Remove handleResponse function from global scope
+        delete window.handleResponse;
+      };
+    }, []);
+  
+    return (
+      <div dangerouslySetInnerHTML={{__html: content}}></div>
     );
 }
