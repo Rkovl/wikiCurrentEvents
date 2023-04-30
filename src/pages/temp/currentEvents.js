69c746e97f88ffb150a670a710e50ebb8a1c405f
RKovl <ryan.kovlaske@gmail.com>
2023-04-29 17:44:23 -0700

    made the backround nicer

--- src/pages/temp/currentEvents.js
+++ src/pages/temp/currentEvents.js
@@ -53,7 +53,7 @@ export default function currentEvents() {
     }, []);
   
     return ( <>
-        <div className='antialiased text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900'>
+        <div className='antialiased text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900 dark:bg-gradient-to-tr from-slate-900 from-50% via-slate-800 via-80% to-slate-900 to-90%'>
           <div className='py-3 border-b border-slate-900/10 lg:px-8 lg:border-1 dark:border-slate-300/10 mx-4 lg:mx-0 mb-3'>
             <div className='relative flex items-center'>
               <div className='mx-auto text-base font-bold tracking-wider uppercase border-2 p-1 border-slate-400'>Current Events</div> 
